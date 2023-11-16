ef2079f3fffec3c4151add619a6ecb68f3de18b3
Matheus Mendes <gynmatheus@hotmail.com.br>
2023-11-16 11:00:21 -0300

    Modulo: Lista de Cursos - Angular - Completo

--- src/app/shared/pipe/category.pipe.ts
+++ src/app/shared/pipe/category.pipe.ts
@@ -9,6 +9,7 @@ export class CategoryPipe implements PipeTransform {
     switch (value) {
       case 'Front-end': return 'code';
       case 'Back-end': return 'computer';
+      case 'Spring': return 'computer';
     }
     return 'code';
   }
